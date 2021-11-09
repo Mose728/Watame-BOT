10312f779206372edf86338619298714b083c83c
Reirel <artolar44@gmail.com>
2021-11-09 17:50:07 -0500

    Iniciando...

---
+++ src/nsfwmenu.js
@@ -0,0 +1,35 @@
+const nsfwmenu = (prefix, pushname) => {
+    return `╭❍ *🌸Watame🌸*
+│
+│✎_𝑴𝒆𝒏𝒖 +18_
+│
+│๛ ${prefix}pussy
+│๛ ${prefix}pussyimage
+│๛ ${prefix}thighs 
+│๛ ${prefix}solo
+│๛ ${prefix}yuri
+│๛ ${prefix}anal
+│๛ ${prefix}pwankg
+│๛ ${prefix}ero1
+│๛ ${prefix}ero2
+│๛ ${prefix}dbz
+│๛ ${prefix}ninte
+│๛ ${prefix}hentai
+│๛ ${prefix}blowjob
+│๛ ${prefix}foxgirl
+│๛ ${prefix}pokemon
+│๛ ${prefix}nekopoi
+│๛ ${prefix}futa
+│๛ ${prefix}cum
+│๛ ${prefix}oppai
+│๛ ${prefix}patas
+│๛ ${prefix}genshin
+│๛ ${prefix}kimetsu
+│๛ ${prefix}cosplay 
+│๛ ${prefix}hero
+│
+╰❏`
+
+}
+
+exports.nsfwmenu = nsfwmenu
