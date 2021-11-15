e27dc512279a47371bcbfccef152d9647124ddba
Reirel <81500953+Reirel@users.noreply.github.com>
2021-11-15 11:41:17 -0500

    Update nsfwmenu.js

--- src/nsfwmenu.js
+++ src/nsfwmenu.js
@@ -3,31 +3,31 @@ const nsfwmenu = (prefix, pushname) => {
 │
 │✎_𝑴𝒆𝒏𝒖 +18_
 │
-│๛ ${prefix}pussy
-│๛ ${prefix}pussyimage
-│๛ ${prefix}thighs 
+│๛ ${prefix}ero
+│๛ ${prefix}dbz
+│๛ ${prefix}cum
 │๛ ${prefix}solo
 │๛ ${prefix}yuri
 │๛ ${prefix}anal
-│๛ ${prefix}pwankg
-│๛ ${prefix}ero
-│๛ ${prefix}dbz
+│๛ ${prefix}hero
+│๛ ${prefix}pussy
 │๛ ${prefix}ninte
+│๛ ${prefix}oppai
+│๛ ${prefix}patas
+│๛ ${prefix}pwankg
 │๛ ${prefix}hentai
+│๛ ${prefix}thighs 
 │๛ ${prefix}blowjob
 │๛ ${prefix}foxgirl
 │๛ ${prefix}pokemon
 │๛ ${prefix}nekopoi
-│๛ ${prefix}futa
-│๛ ${prefix}cum
-│๛ ${prefix}oppai
-│๛ ${prefix}patas
 │๛ ${prefix}genshin
 │๛ ${prefix}kimetsu
+│๛ ${prefix}futanari
 │๛ ${prefix}cosplay 
-│๛ ${prefix}hero
-│๛ ${prefix}dragonmaid
 │๛ ${prefix}konosuba
+│๛ ${prefix}pussyimage
+│๛ ${prefix}dragonmaid
 │
 ╰❏`
 
