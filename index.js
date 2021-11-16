f0dfa006210b8d81875637c380148699e99847bc
Reirel <81500953+Reirel@users.noreply.github.com>
2021-11-16 13:49:29 -0500

    Update index.js

--- index.js
+++ index.js
@@ -29,7 +29,6 @@ const ffmpeg = require('fluent-ffmpeg')
 const imageToBase64 = require('image-to-base64')
 const lolis = require('lolis.life')
 const loli = new lolis()
-const hx = require("hxz-api")
 const chalk = require('chalk');
 const { removeBackgroundFromImageFile } = require('remove.bg')
 const { color, bgcolor } = require('./lib/color')
