4aa0f64044f5acb5ce2fe39d2e3a2267dc8f4b3b
Reirel <81500953+Reirel@users.noreply.github.com>
2021-11-22 12:21:43 -0500

    Update idiomas.js

--- src/idiomas.js
+++ src/idiomas.js
@@ -1,59 +1,50 @@
 const idiomas = (prefix, pushname) => {
     return `╭❍ *🌸Watame🌸*
 │
-│✎*Menu de idiomas TTS*
-│
-│๛ 'af': 'Afrikaans',
-│๛ 'sq': 'Albanian',
-│๛ 'ar': 'Arabic',
-│๛ 'hy': 'Armenian',
-│๛ 'ca': 'Catalan',
-│๛ 'zh': 'Chinese',
-│๛ 'zh-cn': 'Chinese (Mandarin/China)',
-│๛ 'zh-tw': 'Chinese (Mandarin/Taiwan)',
-│๛ 'zh-yue': 'Chinese (Cantonese)',
-│๛ 'hr': 'Croatian',
-│๛ 'cs': 'Czech',
-│๛ 'da': 'Danish',
-│๛ 'nl': 'Dutch',
-│๛ 'en': 'English',
-│๛ 'en-au': 'English (Australia)',
-│๛ 'en-uk': 'English (United Kingdom)',
-│๛ 'en-us': 'English (United States)',
-│๛ 'eo': 'Esperanto',
-│๛ 'fi': 'Finnish',
-│๛ 'fr': 'French',
-│๛ 'de': 'German',
-│๛ 'el': 'Greek',
-│๛ 'ht': 'Haitian Creole',
-│๛ 'hi': 'Hindi',
-│๛ 'hu': 'Hungarian',
-│๛ 'is': 'Icelandic',
-│๛ 'id': 'Indonesian',
-│๛ 'it': 'Italian',
-│๛ 'ja': 'Japanese',
-│๛ 'ko': 'Korean',
-│๛ 'la': 'Latin',
-│๛ 'lv': 'Latvian',
-│๛ 'mk': 'Macedonian',
-│๛ 'no': 'Norwegian',
-│๛ 'pl': 'Polish',
-│๛ 'pt': 'Portuguese',
-│๛ 'pt-br': 'Portuguese (Brazil)',
-│๛ 'ro': 'Romanian',
-│๛ 'ru': 'Russian',
-│๛ 'sr': 'Serbian',
-│๛ 'sk': 'Slovak',
-│๛ 'es': 'Spanish',
-│๛ 'es-es': 'Spanish (Spain)',
-│๛ 'es-us': 'Spanish (United States)',
-│๛ 'sw': 'Swahili',
-│๛ 'sv': 'Swedish',
-│๛ 'ta': 'Tamil',
-│๛ 'th': 'Thai',
-│๛ 'tr': 'Turkish',
-│๛ 'vi': 'Vietnamese',
-│๛ 'cy': 'Welsh'
+*│✎Menu de idiomas TTS*
+  │
+  │๛ af: Afrikaans
+  │๛ sq: Albanian
+  │๛ ar: Arabic
+  │๛ hy: Armenian
+  │๛ ca: Catalan
+  │๛ zh: Chinese
+  │๛ hr: Croatian
+  │๛ cs: Czech
+  │๛ da: Danish
+  │๛ nl: Dutch
+  │๛ en: English
+  │๛ eo: Esperanto
+  │๛ fi: Finnish
+  │๛ fr: French
+  │๛ de: German
+  │๛ el: Greek
+  │๛ ht: Haitian Creole
+  │๛ hi: Hindi
+  │๛ hu: Hungarian
+  │๛ is: Icelandic
+  │๛ id: Indonesian
+  │๛ it: Italian
+  │๛ ja: Japanese
+  │๛ ko: Korean
+  │๛ la: Latin
+  │๛ lv: Latvian
+  │๛ mk: Macedonian
+  │๛ no: Norwegian
+  │๛ pl: Polish
+  │๛ pt: Portuguese
+  │๛ ro: Romanian
+  │๛ ru: Russian
+  │๛ sr: Serbian
+  │๛ sk: Slovak
+  │๛ es: Spanish
+  │๛ sw: Swahili
+  │๛ sv: Swedish
+  │๛ ta: Tamil
+  │๛ th: Thai
+  │๛ tr: Turkish
+  │๛ vi: Vietnamese
+  │๛ cy: Welsh
 │
 ╰❏`
 
