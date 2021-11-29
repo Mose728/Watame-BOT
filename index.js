4c0dd704a80f8dff6c3edc57ea4705156e8cfeb9
Reirel <81500953+Reirel@users.noreply.github.com>
2021-11-29 12:46:38 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1062,11 +1062,10 @@ if (args.length < 1) return reply('Adjunta el enlace')
 query = args.join(" ")
 const apis3l =['7c6c9a9e1138b473e6c64388','ff8508e71c332b870c1e8a1b','b57c69801b7b3e63b3b3e94c','e07d2ff8ff95d995809ec7b3','99ae3d189586081a2be37357','8cd8a7918eab2510afd496c0','69ca7c7a6206090978680cbe','9e91ba5f31f1eb7c772f3100','715bbe735bd7e25872ef40d6','5453dae015e205a91b6994ff']
 const lolkeyapi = apis3l[Math.floor(Math.random() * apis3l.length)]
-reply ('Espera por favor, esto podría tomar un tiempo.')
 anu = await fetchJson(`https://api.lolhuman.xyz/api/facebook?apikey=${lolkeyapi}&url=${query}`, {method: 'get'})
 wing = ` Pedido entregado 🕊 `
 buffer = await getBuffer(anu.result)
-cnf.sendMessage(from, buffer, video, {mimetype: 'video/mp4', filename: `${anu.result}.mp4`, caption: wing})
+cnf.sendMessage(from, buffer, video, {mimetype: 'video/mp4', filename: `${anu.result}.mp4`, quoted: freply ,caption: wing})
 addFilter(from)
 break
 
