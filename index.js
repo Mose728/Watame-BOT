86dadad4147f645490354ac2dd40f8166e9e8b28
Reirel <81500953+Reirel@users.noreply.github.com>
2021-11-29 14:54:42 -0500

    Update index.js

--- index.js
+++ index.js
@@ -2206,19 +2206,6 @@ cnf.sendMessage(from, buffer1, video, {mimetype: 'video/mp4', filename: `${anu.r
 addFilter(from)
 break
 
-case prefix+'twitter':
-if (isBanned) return  reply(mess.banned)
-if (args.length < 1) return reply('Adjunta el enlace')
-terr = args.join(" ")
-const apis32 =['7c6c9a9e1138b473e6c64388','ff8508e71c332b870c1e8a1b','b57c69801b7b3e63b3b3e94c','e07d2ff8ff95d995809ec7b3','99ae3d189586081a2be37357','8cd8a7918eab2510afd496c0','69ca7c7a6206090978680cbe','9e91ba5f31f1eb7c772f3100','715bbe735bd7e25872ef40d6','5453dae015e205a91b6994ff']
-const lolkeyt = apis32[Math.floor(Math.random() * apis32.length)]
-anu = await fetchJson(`https://api.lolhuman.xyz/api/twitter?apikey=${lolkeyt}&url=${terr}`, {method: 'get'})
-wing = ` Pedido entregado 🕊 `
-buffer = await getBuffer(anu.result.link)
-cnf.sendMessage(from, buffer, video, {mimetype: 'video/mp4', filename: `${anu.result.link}.mp4`, quoted: mek ,caption: wing})
-addFilter(from)
-break
-
 case prefix+'charlie':
 if (isBanned) return  reply(mess.banned)
 if (args.length < 1) return reply('*Has tu pregunta!*')		  
