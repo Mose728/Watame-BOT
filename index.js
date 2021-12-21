18ae805573187c06bcdc7ef1d4ddb65bbb002c7a
Reirel <81500953+Reirel@users.noreply.github.com>
2021-12-21 12:56:35 -0500

    Update index.js

--- index.js
+++ index.js
@@ -2498,7 +2498,8 @@ if (mentioned.length > 1) return reply('etiqueta')
 const palm = ['pal1','pal2','pal3','pal4','pal5','pal6','pal7']
 const palma = palm[Math.floor(Math.random() * palm.length)]				
 result = fs.readFileSync(`./sticker/reac/palm/${palma}.mp4`)
-cnf.sendMessage(from, result, video, { caption: `${pushname} le esta dando unas palmaditas a @${mentioned[0].split('@')[0]}`})
+cnf.sendMessage(from, result, video) 
+cnf.sendMessage(from, `${pushname} le esta dando unas palmaditas a @${mentioned[0].split('@')[0]}`)
 addFilter(from)
 break
 			
